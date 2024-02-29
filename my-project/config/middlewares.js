3d2885b7eadeb5f72407802bbbd6caad56b49f7f
lucieblouzon <127098824+lucieblouzon@users.noreply.github.com>
2024-02-29 17:41:26 +0100

    Update middlewares.js

--- my-project/config/middlewares.js
+++ my-project/config/middlewares.js
@@ -9,4 +9,15 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+{
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
+      formidable: {
+        maxFileSize: 250 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      }
+    }
+}
 ];
